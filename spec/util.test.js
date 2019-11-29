4de0941a20dc48845020c78d7fc6d7bd99474514
Angelos Chalaris <chalarangelo@gmail.com>
2019-11-29 15:34:55 +0200

    Add tests for helpers

--- spec/util.test.js
+++ spec/util.test.js
@@ -129,4 +129,89 @@ describe('lib/util', () => {
     });
 
   });
+
+  describe('helpers', () => {
+    it('getMarkDownAnchor is a function', () => {
+      expect(util.getMarkDownAnchor).toBeInstanceOf(Function);
+    });
+
+    describe('getMarkDownAnchor', () => {
+      it('returns the correct anchor slug for the given title', () => {
+        expect(util.getMarkDownAnchor(' I-am-a badTitle-- ')).toBe('i-am-a-badtitle');
+      });
+    });
+
+    it('objectFromPairs is a function', () => {
+      expect(util.objectFromPairs).toBeInstanceOf(Function);
+    });
+
+    describe('objectFromPairs', () => {
+      it('creates an object from the given key-value pairs', () => {
+        expect(util. objectFromPairs([['a', 1], ['b', 2]])).toEqual({ a: 1, b: 2 });
+      });
+    });
+
+    it('capitalize is a function', () => {
+      expect(util.capitalize).toBeInstanceOf(Function);
+    });
+
+    describe('capitalize', () => {
+      it('capitalizes the first letter of a string', () => {
+        expect(util.capitalize('fooBar')).toBe('FooBar');
+      });
+      it('capitalizes the first letter of a string', () => {
+        expect(util.capitalize('fooBar', true)).toBe('Foobar');
+      });
+      it('works with characters', () => {
+        expect(util.capitalize('#!#', true)).toBe('#!#');
+      });
+      it('works with single character words', () => {
+        expect(util.capitalize('a', true)).toBe('A');
+      });
+    });
+
+    it('prepTaggedData is a function', () => {
+      expect(util.prepTaggedData).toBeInstanceOf(Function);
+    });
+
+    describe('prepTaggedData', () => {
+      it('removes duplicate primary tags', () => {
+        const tagDbData = {
+          'snippetA': ['array', 'function', 'intermediate'],
+          'snippetB': ['array', 'object', 'beginner'],
+        };
+        const outData = util.prepTaggedData(tagDbData);
+        expect(outData.length).toBe(1);
+      });
+
+      it('contains all of the unique primary tags from the data', () => {
+        const tagDbData = {
+          'snippetA': ['array', 'function', 'intermediate'],
+          'snippetB': ['object', 'array', 'beginner'],
+        };
+        const outData = util.prepTaggedData(tagDbData);
+        expect(outData.length).toBe(2);
+      });
+
+      it('alphabetizes the  primary tags from the data', () => {
+        const tagDbData = {
+          'snippetA': ['object', 'array', 'intermediate'],
+          'snippetB': ['array', 'function', 'beginner'],
+        };
+        const outData = util.prepTaggedData(tagDbData);
+        expect(outData).toEqual(['array', 'object']);
+      });
+
+      it('handles the special case of Uncategorized', () => {
+        const tagDbData = {
+          'snippetA': ['object', 'array', 'intermediate'],
+          'snippetB': ['uncategorized', 'function', 'beginner'],
+          'snippetC': ['utility', 'function', 'beginner'],
+        };
+        const outData = util.prepTaggedData(tagDbData);
+        expect(outData).toEqual(['object', 'utility', 'uncategorized']);
+      });
+
+    });
+  });
 });
