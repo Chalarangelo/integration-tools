51c835cc04156b9889cfbcc58b08175b35d9897c
Angelos Chalaris <chalarangelo@gmail.com>
2019-12-10 14:37:26 +0200

    Initial setup for initializer script

---
+++ bin/init.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const { blue, green, red } = require('kleur');
+
+// Log a message when the script is run
+console.log(
+  `${blue(
+    'integration-tools [INIT] '
+  )} Initialization procedure starting...`
+);
+
+
+console.log(
+  `${blue(
+    'integration-tools [EXIT] '
+  )} Initialization procedure exiting...`
+);
